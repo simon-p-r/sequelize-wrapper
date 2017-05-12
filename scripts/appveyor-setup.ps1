f29c1aeb51a45a1c0e3856828dd7a1e1417cdba4
simon-p-r <simon.ricaldone@circabs.com>
2017-05-12 12:42:53 +0100

    fix broken appveyor build

--- scripts/appveyor-setup.ps1
+++ scripts/appveyor-setup.ps1
@@ -6,12 +6,13 @@ Start-Service sqlbrowser
 [reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.SqlWmiManagement") | Out-Null
 
 $wmi = New-Object('Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer')
-$tcp = $wmi.GetSmoObject("ManagedComputer[@Name='${env:computername}']/ServerInstance[@Name='MSSQLSERVER']/ServerProtocol[@Name='Tcp']")
+$tcp = $wmi.GetSmoObject("ManagedComputer[@Name='${env:computername}']/ServerProtocol[@Name='Tcp']")
 $tcp.IsEnabled = $true
 $tcp.Alter()
 
 $wmi = New-Object('Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer')
-$ipall = $wmi.GetSmoObject("ManagedComputer[@Name='${env:computername}']/ServerInstance[@Name='MSSQLSERVER']/ServerProtocol[@Name='Tcp']/IPAddress[@Name='IPAll']")
+$ipall = $wmi.GetSmoObject("ManagedComputer[@Name='${env:computername}']/ServerProtocol[@Name='Tcp']/IPAddress[@Name='IPAll']")
+# $ipall = $wmi.GetSmoObject("ManagedComputer[@Name='${env:computername}']/ServerInstance[@Name='MSSQLSERVER']/ServerProtocol[@Name='Tcp']/IPAddress[@Name='IPAll']")
 $port = $ipall.IPAddressProperties.Item("TcpDynamicPorts").Value
 
 $config = @{
